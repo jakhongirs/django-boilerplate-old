a35011ce81e38d0da8ad1f8490c2a956ce016c97
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-21 15:24:12 +0500

    initial

--- core/local_settings.example.py
+++ core/local_settings.example.py
@@ -15,18 +15,18 @@ DATABASES = {
     }
 }
 
-
 HOST = "http://localhost:8000"
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
-        "KEY_PREFIX": "lenta",
-    },
-}
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+#
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+#         "KEY_PREFIX": "lenta",
+#     },
+# }
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
 
 # # CELERY
